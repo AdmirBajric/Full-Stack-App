d337d0d55a96d3b08d1c7b665ec05f3ef5407fa7
Admir Bajric <admir.bajric.dev@gmail.com>
2020-08-23 18:49:05 +0200

    Updates

---
+++ precache-manifest.ca7a27795b6d26496af73d87287385de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bdcd2a536b56543aebc6079f4e97c0",
+    "url": "/Full-Stack-App/index.html"
+  },
+  {
+    "revision": "6f7476e18e8bd39a8e81",
+    "url": "/Full-Stack-App/static/css/main.7097ff2f.chunk.css"
+  },
+  {
+    "revision": "e6bc55304a1561ef8565",
+    "url": "/Full-Stack-App/static/js/2.d970db76.chunk.js"
+  },
+  {
+    "revision": "de81bfc3d566733bec20fcc6cadc32a6",
+    "url": "/Full-Stack-App/static/js/2.d970db76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7476e18e8bd39a8e81",
+    "url": "/Full-Stack-App/static/js/main.5b70cd41.chunk.js"
+  },
+  {
+    "revision": "35f50d486d0f67abc23a",
+    "url": "/Full-Stack-App/static/js/runtime-main.5b97fa93.js"
+  }
+]);
